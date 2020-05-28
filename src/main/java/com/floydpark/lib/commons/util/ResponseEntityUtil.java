5077130ccea51fa1fe110fb3ec2ff4641d5ae824
Hernan Jimenez <Hernan.Jimenez@wnco.com>
2020-05-28 14:56:13 -0500

    add handlers ok responses

--- src/main/java/com/floydpark/lib/commons/util/ResponseEntityUtil.java
+++ src/main/java/com/floydpark/lib/commons/util/ResponseEntityUtil.java
@@ -1,10 +1,13 @@
 package com.floydpark.lib.commons.util;
 
 import com.floydpark.lib.commons.dto.ErrorDTO;
+import com.floydpark.lib.commons.dto.ResponseDTO;
 import com.floydpark.lib.commons.exception.IFloydparkException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 public enum ResponseEntityUtil {
 
@@ -15,5 +18,24 @@ public enum ResponseEntityUtil {
         ErrorDTO errorDTO = new ErrorDTO();
         errorDTO.setErrors(e.getErrors());
         return new ResponseEntity<>(errorDTO, e.getErrorHttpStatus());
+    }
+
+    public ResponseEntity<Serializable> handleSingleResponse(Serializable response){
+
+        return handleSingleResponse(response, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Serializable> handleSingleResponse(Serializable response, HttpStatus httpStatus){
+
+        ResponseDTO<Serializable> responseDTO = new ResponseDTO<>();
+        responseDTO.setData(response);
+        return new ResponseEntity<>(responseDTO, httpStatus);
+    }
+
+    public ResponseEntity<Serializable> handleCollectionResponse(Collection response){
+
+        ResponseDTO<Collection> responseDTO = new ResponseDTO<>();
+        responseDTO.setData(response);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 }
